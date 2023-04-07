c0475a3015361e09b317f66544fb60574a3092e8
isahua16 <isaelhuard@gmail.com>
2023-04-06 19:39:13 -0600

    Post form POST request

--- js/app.js
+++ js/app.js
@@ -1,3 +1,45 @@
+function submit_post(event)
+{
+    if(document.querySelectorAll(`.msg`))
+    {
+        for(let i = 0; i < document.querySelectorAll(`.msg`).length; i++)
+        {
+            document.querySelectorAll(`.msg`)[i].remove();
+        }
+    }
+    
+    let post_title = document.querySelector(`#post_title`)[`value`];
+    let post_body = document.querySelector(`#post_body`)[`value`];
+    let post_id = document.querySelector(`#user_id`)[`value`];
+
+    axios.request(
+        {
+            url: `https://jsonplaceholder.typicode.com/posts`,
+            method: `POST`,
+            data:
+            {
+                title: post_title,
+                body: post_body,
+                userId: post_id
+            }
+        }
+    ).then(post_success).catch(post_error);
+}
+
+function post_success(res)
+{
+    post_button.insertAdjacentHTML(`afterend`, `<h3 class="msg">Posted Succesfully!</h3>`);
+    document.querySelector(`#post_title`)[`value`] = ``;
+    document.querySelector(`#post_body`)[`value`] = ``;
+    document.querySelector(`#user_id`)[`value`] = ``;
+       
+}
+
+function post_error(err)
+{
+    post_button.insertAdjacentHTML(`afterend`, `<h3 class="msg">There was an error. Try again.</h3>`);
+}
+
 function press_enter(event)
 {
     if(event[`key`] === `Enter`)
@@ -8,7 +50,6 @@ function press_enter(event)
 
 function meal_search(event)
 {
-    
     recipe_container.innerHTML = ``;
     let param = search_box[`value`];
     axios.request(
@@ -39,10 +80,10 @@ function get_recipe_failure(err)
     recipe_container.insertAdjacentHTML(`beforeend`, `<p>Oops, an error occured!</p>`)
 }
 
-
 let recipe_container = document.querySelector(`#recipe_container`);
 let search_button = document.querySelector(`#search_button`);
 search_button.addEventListener(`click`, meal_search);
 let search_box = document.querySelector(`#search_box`);
 search_box.addEventListener(`keydown`, press_enter);
-
+let post_button = document.querySelector(`#post_button`);
+post_button.addEventListener(`click`, submit_post);
